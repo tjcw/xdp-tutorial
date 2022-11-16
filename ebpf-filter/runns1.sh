72d48262bf914cdcc382a06866033d5f0d475516
Chris Ward <tjcw@uk.ibm.com>
2022-11-16 14:39:46 +0000

    Longer wait for startup

--- ebpf-filter/runns1.sh
+++ ebpf-filter/runns1.sh
@@ -5,6 +5,6 @@ ip netns exec ns1 ip link set lo up
 ip netns exec ns1 ip link set vpeer1 up
 
 ip netns exec ns1 ip addr add 10.10.0.10/16 dev vpeer1
-sleep 2
+sleep 6
 ip netns exec ns1 ping -c 5 10.10.0.20
 
