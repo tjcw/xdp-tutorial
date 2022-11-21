d55ad94108d8c74de1992e03dd55df86081265fb
Chris Ward <tjcw@uk.ibm.com>
2022-11-21 08:42:40 -0500

    Already in namespace for runns1.sh

--- ebpf-filter/runns1.sh
+++ ebpf-filter/runns1.sh
@@ -1,10 +1,8 @@
 #!/bin/bash -x
 
-ip netns exec ns1 ip link set lo up
-
-ip netns exec ns1 ip link set vpeer1 up
-
-ip netns exec ns1 ip addr add 10.10.0.10/16 dev vpeer1
+ip link set lo up
+ip link set vpeer1 up
+ip addr add 10.10.0.10/16 dev vpeer1
 sleep 6
-ip netns exec ns1 ping -c 10 10.10.0.20
+ping -c 10 10.10.0.20
 
